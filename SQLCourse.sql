a3df600c8028cf7033648c6070326ae6773a3f91
shivam0277 <shivam02774@gmail.com>
2026-01-02 21:55:28 +0530

    Add VSCode SQL settings and update SQL course files
    Added .vscode/settings.json for SQLTools MySQL connection. Replaced SQLVideoCourse.sql and 1.sql with SQLCourse.sql containing a brief list of relational database distributions.

---
+++ SQLCourse.sql
@@ -0,0 +1,10 @@
+-- There are several relational database software packages. Some are free and open source, while others are paid 
+-- and proprietary. Here some popular relational database distributions:
+-- MySQL
+-- Postgres
+-- SQLite
+-- Microsoft SQL Server
+-- MariaDB
+-- Oracle
+-- IBM DB2
+
